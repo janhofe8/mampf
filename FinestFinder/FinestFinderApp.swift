e63f510045939be62001ec46efefbd98b775b41e
Jan Hoferichter <j.hoferichter@web.de>
2026-03-19 18:06:27 +0100

    Initial Commit

---
+++ FinestFinder/FinestFinderApp.swift
@@ -0,0 +1,32 @@
+//
+//  FinestFinderApp.swift
+//  FinestFinder
+//
+//  Created by Jan Hoferichter on 19.03.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FinestFinderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FinestFinderTests/FinestFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinestFinderTests.swift
+//  FinestFinderTests
+//
+//  Created by Jan Hoferichter on 19.03.26.
+//
+
+import Testing
+@testable import FinestFinder
+
+struct FinestFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
